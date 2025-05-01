8aa5bcb807d45f1516d9f69483754c5b9000b6a2
Aditya <adityasammi169@gmail.com>
2025-05-01 15:26:23 +0530

    Speed Update

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Layout from "./Layout";
 import Hero from "./components/Hero/Hero";
 import Cv from "./components/Pages/Cv"; 
@@ -36,6 +37,7 @@ const router = createBrowserRouter(
 createRoot(document.getElementById("root")).render(
   <StrictMode>
      <Analytics />
+     <SpeedInsights />
     <RouterProvider router={router} />
   </StrictMode>
 );
